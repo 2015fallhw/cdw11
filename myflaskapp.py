babfc06ba7ca79abf9681f04d4148dee246abd18
40323105 <40323105@gm.nfu.edu.tw>
2016-06-11 13:46:11 +0800

    0610-add 40323105-1

--- myflaskapp.py
+++ myflaskapp.py
@@ -13,8 +13,6 @@ import users.a.g10.ag10_40323139
 import users.a.g9.ag9_40323132task
 import users.a.g5.ag5_40323113task1
 import users.a.g5.ag5_40123145task1
-import users.a.g5.ag5_40123145task2
-import users.a.g5.ag5_40123145task3
 import users.a.g4.ag4_40323138
 import users.a.g4.ag4_40323138_task2bacd
 import users.a.g4.ag4_40323138_task3abcd
@@ -25,9 +23,7 @@ import users.a.g1.a40223153
 import users.a.g3.ag3_40323108
 import users.a.g3.ag3_40323108_task2bacd
 import users.a.g3.ag3_40323108_task3abcd
-import users.a.g4.ag4_40323109
-import users.a.g4.ag4_40323109_task2bacd
-import users.a.g4.ag4_40323109_task3abcd
+import users.a.g1.a40323105_1
 #bg1
 import users.b.g1.bg1_40123156
 import users.b.g1.bg1_40123156_2
@@ -93,7 +89,6 @@ import users.a.g7.ag7_40123149_1
 import users.a.g7.ag7_40123149_2
 #ag8
 import users.a.g8.a40323143
-import users.a.g8.a40323137
 import users.a.g8.a40323154
 #ag10
 import users.a.g10.a40323139
@@ -488,8 +483,6 @@ app.register_blueprint(users.a.g9.ag9_40323132task.ag9_40323132)
 app.register_blueprint(users.a.g10.ag10_40323139.ag10_40323139)
 app.register_blueprint(users.a.g5.ag5_40323113task1.ag5_40323113)
 app.register_blueprint(users.a.g5.ag5_40123145task1.ag5_40123145)
-app.register_blueprint(users.a.g5.ag5_40123145task2.ag5_40123145_2)
-app.register_blueprint(users.a.g5.ag5_40123145task3.ag5_40123145_3)
 app.register_blueprint(users.a.g8.ag8_40323131_task1.ag8_40323131)
 app.register_blueprint(users.a.g4.ag4_40323138_task2bacd.ag4_40323138bacd)
 app.register_blueprint(users.a.g4.ag4_40323138_task3abcd.ag4_40323138task3)
@@ -501,11 +494,10 @@ app.register_blueprint(users.a.g1.a40223153.ag1_40223153)
 app.register_blueprint(users.a.g3.ag3_40323108.ag3_40323108)
 app.register_blueprint(users.a.g3.ag3_40323108_task2bacd.ag3_40323108bacd)
 app.register_blueprint(users.a.g3.ag3_40323108_task3abcd.ag3_40323108task3)
-app.register_blueprint(users.b.g9.bg9_40323250.bg9_40323250)
+app.register_blueprint(users.a.g1.a40323105_1.a40323105_1)
 
-app.register_blueprint(users.a.g4.ag4_40323109.ag4_40323109)
-app.register_blueprint(users.a.g4.ag4_40323109_task2bacd.ag4_40323109bacd)
-app.register_blueprint(users.a.g4.ag4_40323109_task3abcd.ag4_40323109task3)
+
+app.register_blueprint(users.b.g9.bg9_40323250.bg9_40323250)
 #bg1
 app.register_blueprint(users.b.g1.bg1_40123156.bg1_40123156)
 app.register_blueprint(users.b.g1.bg1_40123156_2.bg1_40123156_2)
@@ -572,8 +564,7 @@ app.register_blueprint(users.b.g101.b40123200.bg101)
 app.register_blueprint(users.a.g7.ag7_40123149_1.ag7_40123149_1)
 app.register_blueprint(users.a.g7.ag7_40123149_2.ag7_40123149_2)
 #ag8
-#app.register_blueprint(users.a.g8.a40323143.ag8_40323143)
-app.register_blueprint(users.a.g8.a40323137.ag8_40323137)
+
 app.register_blueprint(users.a.g8.a40323154.ag8_40323154)
 #ag10
 app.register_blueprint(users.a.g10.a40323139.ag10_40323139_1)
