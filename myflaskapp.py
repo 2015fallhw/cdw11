0a44bec9132fb3c832fbe66f8002797750693282
40323252 <40323252@gm.nfu.edu.tw>
2016-06-16 09:52:22 +0800

    40323252

--- myflaskapp.py
+++ myflaskapp.py
@@ -83,6 +83,7 @@ import users.b.g5.b40323206_cdw11_2
 import users.b.g11.bg11_40323245
 import users.b.g11.bg11_40323252
 import users.b.g11.bg11_40323245_1
+import users.b.g11.b40323252
 #bg101
 import users.b.g101.b40123200
 #ag7
@@ -567,6 +568,7 @@ app.register_blueprint(users.b.g5.b40323206_cdw11_2.bg5_40323206_2)
 app.register_blueprint(users.b.g11.bg11_40323245.bg11_40323245)
 app.register_blueprint(users.b.g11.bg11_40323252.bg11_40323252)
 app.register_blueprint(users.b.g11.bg11_40323245_1.bg11)
+app.register_blueprint(users.b.g11.b40323252.bg11_1)
 #bg101
 app.register_blueprint(users.b.g101.b40123200.bg101)
 
