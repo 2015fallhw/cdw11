13c9fd2cc3a5d8301c17e65e61a222d12529ed56
dora40323106 <40323106@gm.nfu.edu.tw>
2016-06-16 23:24:11 +0800

    40323145

--- myflaskapp.py
+++ myflaskapp.py
@@ -104,6 +104,7 @@ import users.a.g7.ag7_40123149_2
 import users.a.g8.a40323143
 import users.a.g8.a40323137
 import users.a.g8.a40323154
+import users.a.g8.ag8_40323145
 #ag10
 import users.a.g10.a40323139
 import users.a.g10.a40323141
@@ -595,6 +596,7 @@ app.register_blueprint(users.a.g7.ag7_40123149_2.ag7_40123149_2)
 #app.register_blueprint(users.a.g8.a40323143.ag8_40323143)
 app.register_blueprint(users.a.g8.a40323137.ag8_40323137)
 app.register_blueprint(users.a.g8.a40323154.ag8_40323154)
+app.register_blueprint(users.a.g8.ag8_40323145.ag8_40323145)
 #ag10
 app.register_blueprint(users.a.g10.a40323139.ag10_40323139_1)
 #app.register_blueprint(users.a.g10.a40323141.ag10_40323141_1)
