25ffd29229b011d78ac39373e9659075e1b02056
40123144 <40123144@gm.nfu.edu.tw>
2016-06-16 09:54:03 +0800

    40123144

--- myflaskapp.py
+++ myflaskapp.py
@@ -82,7 +82,6 @@ import users.b.g5.b40323206_cdw11_2
 #bg11
 import users.b.g11.bg11_40323245
 import users.b.g11.bg11_40323252
-import users.b.g11.bg11_40323245_1
 #bg101
 import users.b.g101.b40123200
 #ag7
@@ -566,7 +565,7 @@ app.register_blueprint(users.b.g5.b40323206_cdw11_2.bg5_40323206_2)
 #bg11
 app.register_blueprint(users.b.g11.bg11_40323245.bg11_40323245)
 app.register_blueprint(users.b.g11.bg11_40323252.bg11_40323252)
-app.register_blueprint(users.b.g11.bg11_40323245_1.bg11)
+#app.register_blueprint(users.b.g11.bg11.bg11_40323245_1)
 #bg101
 app.register_blueprint(users.b.g101.b40123200.bg101)
 
