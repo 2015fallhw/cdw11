8fb064af27ff36d9424b5a1a142bc587265e084a
40123149 <40123149@gm.nfu.edu.tw>
2016-06-20 10:14:47 +0800

    修改myflaskapp檔

--- myflaskapp.py
+++ myflaskapp.py
@@ -119,6 +119,7 @@ import users.b.g101.b40123200
 #ag7
 import users.a.g7.ag7_40123149_1
 import users.a.g7.ag7_40123149_2
+import users.a.g7.ag7_40123149_3
 #ag8
 import users.a.g8.a40323143
 import users.a.g8.a40323137
@@ -634,6 +635,7 @@ app.register_blueprint(users.b.g101.b40123200.bg101)
 #ag7
 app.register_blueprint(users.a.g7.ag7_40123149_1.ag7_40123149_1)
 app.register_blueprint(users.a.g7.ag7_40123149_2.ag7_40123149_2)
+app.register_blueprint(users.a.g7.ag7_40123149_3.ag7_40123149_3)
 #ag8
 app.register_blueprint(users.a.g8.a40323143.ag8_40323143)
 app.register_blueprint(users.a.g8.a40323137.ag8_40323137)
