08070e90506ccf66333da3d6d60d5bb61050fb94
40123134 <40123134@gm.nfu.edu.tw>
2016-06-20 18:29:48 +0800

    40123134

--- users/b/g1/bg1_40123134_3.py
+++ users/b/g1/bg1_40123134_3.py
@@ -392,9 +392,9 @@ def eighteenthirty(x, y):
     x = 50
     y = 0
     degree = 20
-    first_degree = 20.78
-    startx = -233.06+100
-    starty = 49.48
+    first_degree = 20.78+90
+    startx = 44
+    starty = -165
     repeat = 360 / degree
     # 先畫出左邊第一關鍵節
     outstring = '''
@@ -408,17 +408,17 @@ x1, y1 = mychain.basic_rot('''+str(startx)+","+str(starty)+", "+str(first_degree
         if i >=2 and i <=11:
             # virautl chain
             #outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
-            outstring += "x"+str(i)+", -50"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", -50"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+", True) \n"
         else:
-            outstring += "x"+str(i)+", -50"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", -50"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
+            outstring += "x"+str(i)+", y"+str(i)+"=mychain.basic_rot(x"+str(i-1)+", y"+str(i-1)+","+str(first_degree+degree-i*degree)+") \n"
  
     # 接著處理右邊的非虛擬鍊條
     # 先畫出右邊第一關鍵節
  
-    p = -17.89
-    k = 93.98
+    p = 0
+    k = 50
     degree = 12
-    first_degree = 4.78
+    first_degree = 4.78+90
     repeat = 360 / degree
     # 第1節不是 virtual chain
     outstring += '''
@@ -436,7 +436,7 @@ p1, k1 = mychain.basic_rot('''+str(p)+","+str(k)+", "+str(first_degree)+''')
  
     # 上段連接直線
     # 從 x1, y1 作為起點
-    first_degree = 10.78
+    first_degree = 10.78 +90 
     repeat = 10
     outstring += '''
 m1, n1 = mychain.basic_rot(x1, y1, '''+str(first_degree)+''')
@@ -446,7 +446,7 @@ m1, n1 = mychain.basic_rot(x1, y1, '''+str(first_degree)+''')
  
     # 下段連接直線
     # 從 x11, y11 作為起點
-    first_degree = -10.78
+    first_degree = -10.78 +90
     repeat = 10
     outstring += '''
 r1, s1 = mychain.basic_rot(x11, y11, '''+str(first_degree)+''')
